d8715fe570832beb879854bec08c113cc79f7662
孙凯峰 <sunkaifeng@sunkaifengdemacbook-pro.local>
2016-07-09 14:47:42 +0800

    Initial Commit

---
+++ SKFHorizontalScreenTests/SKFHorizontalScreenTests.m
@@ -0,0 +1,39 @@
+//
+//  SKFHorizontalScreenTests.m
+//  SKFHorizontalScreenTests
+//
+//  Created by 孙凯峰 on 16/7/9.
+//  Copyright © 2016年 孙凯峰. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface SKFHorizontalScreenTests : XCTestCase
+
+@end
+
+@implementation SKFHorizontalScreenTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
